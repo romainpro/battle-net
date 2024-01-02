290373a1099e2141723ad3750dee871d3f2fed71
romain <romain.casagrande.pro@gmail.com>
2024-01-02 13:40:19 +0100

    Ui-Button-SizeProps-switch

--- src/design/Button/Button.tsx
+++ src/design/Button/Button.tsx
@@ -9,8 +9,26 @@ interface PropsButton{
 
 
 export const Button =({size="medium"}:PropsButton)=>{
+
+    let varianteSize:string=""
+
+
+    switch(size){
+        case "small":
+            varianteSize="w-[225px] h-[72px]"
+            break
+        case "medium"://default
+            varianteSize="w-[280px] h-[52px]"
+            break
+        case "large":
+            varianteSize="w-[450px] h-[72px]"
+            break
+    }
+
+
+
     return(
-        <button className={clsx()}>
+        <button className={clsx(varianteSize)}>
 
         </button>
     )
